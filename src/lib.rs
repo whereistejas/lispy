1896e1ef4f60154493932a391dba41ebc2cd46c8
Tejas Sanap <email@whereistejas.xyz>
2023-12-14 03:08:33 +0530

    Create types.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,45 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#![allow(dead_code)]
+
+#[derive(Debug, PartialEq)]
+enum Number {
+    Int(u32),
+    Float(f32),
+}
+#[derive(Debug, PartialEq)]
+enum Atom {
+    Symbol(String),
+    Number(Number),
+}
+#[derive(Debug, PartialEq)]
+enum Expr<T> {
+    Atom(Atom),
+    List(Vec<T>),
+}
+#[derive(Debug, PartialEq)]
+enum T {
+    Symbol(String),
+    Number(Number),
+    Atom(Atom),
+    List(Vec<T>),
+    Expr(Expr<T>),
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+fn tokenize(s: &str) -> Vec<String> {
+    s.replace("(", " ( ")
+        .replace(")", " ) ")
+        .split(" ")
+        .map(ToOwned::to_owned)
+        .collect()
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+fn parse_from_tokens(tokens: Vec<String>) -> T {
+    todo!()
+}
+
+#[test]
+fn expected_to_panic() {
+    let s = "(begin (define r 10) (* pi (* r r)))";
+    let t = parse_from_tokens(tokenize(s));
+
+    assert_eq!(t, T::Symbol("abc".to_string()))
 }
