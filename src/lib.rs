620e6967ff0db642d26bd6fc4206314ad79a243f
Tejas Sanap <email@whereistejas.xyz>
2023-12-14 18:41:13 +0530

    working parser

--- src/lib.rs
+++ src/lib.rs
@@ -28,18 +28,67 @@ fn tokenize(s: &str) -> Vec<String> {
     s.replace("(", " ( ")
         .replace(")", " ) ")
         .split(" ")
-        .map(ToOwned::to_owned)
+        .filter_map(|s| {
+            if s.is_empty() {
+                None
+            } else {
+                Some(s.to_owned())
+            }
+        })
         .collect()
 }
 
-fn parse_from_tokens(tokens: Vec<String>) -> T {
-    todo!()
+fn parse_atom(atom: &str) -> T {
+    atom.parse::<u32>()
+        .map(|i| T::Number(Number::Int(i)))
+        .unwrap_or_else(|_| {
+            atom.parse::<f32>()
+                .map(|f| T::Number(Number::Float(f)))
+                .unwrap_or_else(|_| T::Symbol(atom.to_string()))
+        })
+}
+
+// Keep recursively reading nested statements.
+// Prepare return all tokens when you encounter the first ")".
+fn parse_from_tokens(tokens: Vec<&str>) -> T {
+    match tokens[..] {
+        [] => panic!("unexpected EOF"),
+        ["(", ..] => {
+            let mut tokens = tokens.into_iter().skip(1).collect::<Vec<_>>();
+            let mut l = vec![];
+
+            while tokens.first() != Some(&")") {
+                let token = parse_from_tokens(tokens.clone());
+                l.push(token);
+                tokens = tokens.into_iter().skip(1).collect::<Vec<_>>();
+            }
+
+            T::Expr(Expr::List(l))
+        }
+        [token, ..] => parse_atom(token),
+    }
 }
 
 #[test]
-fn expected_to_panic() {
-    let s = "(begin (define r 10) (* pi (* r r)))";
-    let t = parse_from_tokens(tokenize(s));
+fn tokenize_and_parse() {
+    let t = parse_from_tokens(
+        tokenize("(begin (define r 10) (* pi (* r r)))")
+            .iter()
+            .map(|s| &**s)
+            .collect(),
+    );
 
-    assert_eq!(t, T::Symbol("abc".to_string()))
+    let e = T::Expr(Expr::List(vec![
+        T::Symbol("begin".to_string()),
+        T::Expr(Expr::List(vec![
+            T::Symbol("define".to_string()),
+            T::Symbol("r".to_string()),
+            T::Number(Number::Int(10)),
+        ])),
+        T::Symbol("define".to_string()),
+        T::Symbol("r".to_string()),
+        T::Number(Number::Int(10)),
+    ]));
+
+    assert_eq!(t, e, "{t:#?}")
 }
