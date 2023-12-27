8b1b2b03b0f59b500175552ad3d79695f6b2839e
Tejas Sanap <email@whereistejas.xyz>
2023-12-27 18:20:06 +0530

    Make everything smaller

--- lib/lispy.ml
+++ lib/lispy.ml
@@ -77,39 +77,39 @@ module Types = struct
     F.pp_print_list ~pp_sep:(fun fmt () -> F.fprintf fmt " ") (fun fmt x -> pp fmt x) e
   ;;
 
-  let rec parse_token token acc tokens =
-    match tokens with
-    | [] -> Error (Failure "Unexpected EOF")
-    | t :: tl when t = token -> Ok (Atom t :: acc, tl)
-    | t :: _ -> Error (unexpected_token token t)
-
-  and try_parse_atom acc tokens =
-    match tokens with
-    | [] -> Error (Failure "Unexpected EOF")
-    | Tokens.OP :: _ -> try_parse_expr acc tokens
-    | (Tokens.S _ as token) :: rest -> Ok (Atom token :: acc, rest)
-    | (Tokens.N _ as token) :: rest -> Ok (Atom token :: acc, rest)
-    | t :: _ -> Error (unexpected_token (Tokens.S "Symbol|Number") t)
-
-  and try_parse_expr acc tokens =
-    let ( let* ) x f =
-      match x with
-      | Ok x -> f x
-      | Error _ as e -> e
+  let parse tokens =
+    let rec helper acc tokens =
+      match tokens with
+      | [] -> raise (Failure "Unexpected EOF")
+      | t ->
+        (match parse_expr acc t with
+         | Ok (parsed, _rest) -> Expr parsed
+         | Error exn -> raise exn)
+    and parse_expr acc tokens =
+      let ( let* ) x f =
+        match x with
+        | Ok x -> f x
+        | Error _ as e -> e
+      in
+      let* parsed, rest = parse_token Tokens.OP [] tokens in
+      let* parsed, rest = parse_atom parsed rest in
+      let* parsed, rest = parse_atom parsed rest in
+      let* parsed, rest = parse_atom parsed rest in
+      let* parsed, rest = parse_token Tokens.CP parsed rest in
+      Ok (Expr (List.rev parsed) :: acc, rest)
+    and parse_token token acc tokens =
+      match tokens with
+      | [] -> Error (Failure "Unexpected EOF")
+      | t :: tl when t = token -> Ok (Atom t :: acc, tl)
+      | t :: _ -> Error (unexpected_token token t)
+    and parse_atom acc tokens =
+      match tokens with
+      | [] -> Error (Failure "Unexpected EOF")
+      | Tokens.OP :: _ -> (parse_expr [@tailcall]) acc tokens
+      | (Tokens.S _ as token) :: rest -> Ok (Atom token :: acc, rest)
+      | (Tokens.N _ as token) :: rest -> Ok (Atom token :: acc, rest)
+      | t :: _ -> Error (unexpected_token (Tokens.S "Symbol|Number") t)
     in
-    let* parsed, rest = parse_token Tokens.OP [] tokens in
-    let* parsed, rest = try_parse_atom parsed rest in
-    let* parsed, rest = try_parse_atom parsed rest in
-    let* parsed, rest = try_parse_atom parsed rest in
-    let* parsed, rest = parse_token Tokens.CP parsed rest in
-    Ok (Expr parsed :: acc, rest)
-
-  and parse tokens =
-    match tokens with
-    | [] -> raise (Failure "Unexpected EOF")
-    | t ->
-      (match try_parse_expr [] t with
-       | Ok (parsed, _rest) -> Expr parsed
-       | Error exn -> raise exn)
+    helper [] tokens
   ;;
 end
